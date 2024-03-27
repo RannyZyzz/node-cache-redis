a040d052c73d8f00388bc7c0e2f03936f82d7074
ranny-pop-os-22-04 <rannierreis@gmail.com>
2024-03-26 22:56:20 -0300

    inclui novas rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,22 @@
 import { Router } from "express";
+import { authentication } from "./middleware/auth";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { LoginUserController } from "./controllers/LoginUsersControllers";
+import { GetUserInfoController } from "./controllers/GetUserInfoController";
+
 
 const router = Router();
 
 const createUserController = new CreateUserController()
+const loginUserController = new LoginUserController()
+const getUserInfoController = new GetUserInfoController()
+
 router.get('/', (request, response) => {
     response.send("I'm ok")
 })
 
 router.post('/users', createUserController.handle)
+router.post('/login', loginUserController.handle)
+router.get('/users/profile', authentication, getUserInfoController.handle)
 
 export default router
