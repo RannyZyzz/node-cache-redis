c7ad77eae1f9f5f72b41efe01d7b36df54566841
ranny-pop-os-22-04 <rannierreis@gmail.com>
2024-03-29 20:10:48 -0300

    implementa recuperação de dados do beneficiario no banco redis apartir da chave userId

--- src/controllers/GetUserInfoController.ts
+++ src/controllers/GetUserInfoController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+import { getRedis } from '../redisConfig';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 export class GetUserInfoController {
     async handle(request: Request, response: Response) {
@@ -9,14 +10,21 @@ export class GetUserInfoController {
 
         console.time()
 
-        const user = await prisma.users.findUnique({
-            where: {
-                id: userId
-            }
-        })
+        const userRedis = await getRedis(`user-${userId}`)
+
+        const userJson = JSON.parse(userRedis)
+
+        // const user = await prisma.users.findUnique({
+        //     where: {
+        //         id: userId
+        //     }
+        // })
 
         console.timeEnd()
 
-        return response.json(user)
+
+        return response.json(userJson)
+
+
     }
 }
